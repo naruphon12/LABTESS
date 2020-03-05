45a7bdd46da6c0ca9e256b056312afc1ce6add13
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-03-05 12:45:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,31 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name "viewport" content "width device-width,initial-scale 1.0">
 	<title>Document</title>
+	<script str ="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
+	<script str ="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			liff 
+			.init({
+                liffid:'1653805513-OWPbPoe0'
+            })
+            .then())=>{
+                liff.getprofile().then(function (profile) {
+                    alert(JSON.(stringify(profile));
+                });
+                })
+                .catch((err)=>{
+                    alert(JSON.(stringify(err));
+                });
+            });
+            </script>
+				
+			
+		
 </head>
 <body>
-	<h1>Welcome Heroku ball</h1>
+<h1>Welcome Heroku ball</h1>
 </body>
 </html>
