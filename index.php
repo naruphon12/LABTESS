a0241babf1b13b5dbec04321abffcffb644c9bea
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-03-05 14:33:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 	<meta charset="UTF-8">
 	<meta name "viewport" content "width device-width,initial-scale 1.0">
 	
-	<script scr ="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
-	<script scr ="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<script src ="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
+	<script src ="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 	<script>
 		$(document).ready(function(){
 			liff 
@@ -27,6 +27,6 @@
 		
 
 
-<h1>Welcome Heroku ball2</h1>
+<h1>Welcome Heroku ball7</h1>
 </body>
 </html>
