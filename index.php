83213cca9951bdc10b3630cbf567ef59a7300275
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-03-08 14:34:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -33,6 +33,20 @@
     }, err => console.error(err.code, error.message));
   </script>
   	<script>
+    POST :"http://localhost:52108/WebService.asmx"
+Host: localhost
+Content-Type: text/xml; charset=utf-8
+Content-Length: length
+SOAPAction: "http://tempuri.org/pdftojpg"
+
+<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <pdftojpg xmlns="http://tempuri.org/">
+      <JsonStr>{"Data": [{"parth": "Vm-feeduatvb_net_project$E-Document","name": "Vm-feeduatvb_net_project$E-Document"}]}</JsonStr>
+    </pdftojpg>
+  </soap:Body>
+</soap:Envelope>
 	  function submit_soap(){
 		var key1=$("#key1").val();
 		var key2=$("#key2").val();
@@ -46,8 +60,8 @@
    <center>
     <h3>Send HTTP POST Request using PHP</h3>
      <form>
-     customer_code : <input name="key1" id="key1" type="text" /><br />
-     User_id  : <input name="key2" id="key2" type="text" /><br />
+     Name1 : <input name="key1" id="key1" type="text" /><br />
+     Name2  : <input name="key2" id="key2" type="text" /><br />
       <input type="button" value="Submit" onclick="submit_soap()"/>
     </form>
        <br>-----------
@@ -55,4 +69,3 @@
    </center>
 </body>
 </html>
-
