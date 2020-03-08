5f86db6327c63e9a4694edfa0f3d446ae2526670
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-03-08 14:26:46 +0700

    Create form_post.php

---
+++ form_post.php
@@ -0,0 +1,29 @@
+<?php
+$key1=$_POST['key1'];
+$key2=$_POST['key2'];
+$url = 'https://postman-echo.com/post';
+$data = array('key1' => $key1, 'key2' => $key2);
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$response = file_get_contents($url, false, $context);
+if ($response === FALSE) { /* Handle error */ }
+  $json_array=json_decode($response,true); 
+ function display_array_recursive($json_rec){
+		if($json_rec){
+			foreach($json_rec as $key=> $value){
+				if(is_array($value)){
+					display_array_recursive($value);
+				}else{
+					echo $key.'--'.$value.'<br>';
+				}	
+			}	
+		}	
+	}
+  	display_array_recursive($json_array);
+?>
