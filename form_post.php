7e41fbf2ab569a9afc22c8cd246d45fba8832695
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-04-16 15:24:59 +0700

    Delete form_post.php

--- form_post.php
+++
@@ -1,6 +0,0 @@
-<?php
-$client = new SoapClient("http://testurl/Test.asmx?WSDL");
-$params->Param1 = 'Hello';
-$params->Param2 = 'World!';    
-$result = $client->TestMethod($params)->TestMethodResult;
-?>
