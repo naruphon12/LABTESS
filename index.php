02e13e01f20be59754e51dc084ec8a0048a9b280
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-04-20 13:15:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -53,29 +53,7 @@
         var email = $("#email").val();
         var msg_subject = $("#msg_subject").val();
         var message = $("#message").val();
-        
-	var url = "http://vm-feeduat/FeedLineBot/WebService.asmx";
          $.ajax({
-        type: "POST",
-        url: url + "/registerline",
-        data: "{variant_id:'1'}",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: OnSuccessCall,
-        error: OnErrorCall
-    });
-
-    function OnSuccessCall(response) {
-        alert(response.d);
-    }
-
-
-    function OnErrorCall(response) {
-        alert(response.status + " " + response.statusText);
-    }
-	      
-      
-            $.ajax({
             type: "POST",
             url: "/formProcess.php",
             dataType: "json",
